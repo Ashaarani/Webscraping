67bf1eff0cf5dc254e75eccda8342a62b75e873b
Asha Rani Nagarajan <159388490+Ashaarani@users.noreply.github.com>
2024-07-23 14:09:30 -0400

    Add files via upload
    This script is designed to scrape product information from an Amazon webpage, specifically targeting the title and price of a product, and save this data into a CSV file. The script combines the use of selenium for web automation and BeautifulSoup for parsing HTML content. Here's a step-by-step breakdown of the script:
    
    Import Necessary Libraries:
    
    BeautifulSoup from bs4 for parsing HTML.
    requests for making HTTP requests (though it's not used in this script).
    smtplib for sending emails (also not used here).
    time, datetime, and csv for handling time, dates, and CSV file operations respectively.
    pandas for data manipulation and analysis.
    selenium for web automation.
    Set Up WebDriver:
    
    The script uses the Safari WebDriver to navigate to the target Amazon product page.
    Fetch and Parse Web Page:
    
    The script fetches the page content and parses it using BeautifulSoup.
    It extracts the product title and price using the relevant HTML element IDs.
    Save Data to CSV:
    
    The script writes the extracted data (title, price, date) into a CSV file.
    If the CSV file already exists, it appends the new data.
    Automate Price Checking:
    
    A function check_price() encapsulates the web scraping logic.
    The script enters an infinite loop, calling check_price() every hour to continuously monitor the product price and update the CSV file.
    Display Data:
    
    The script reads the data from the CSV file using pandas and prints it.

---
+++ Amazon web scraping.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[56]:
+
+
+get_ipython().system('pip install bs4')
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import time
+import datetime
+import csv
+import pandas as pd
+
+
+# In[57]:
+
+
+get_ipython().system('pip install selenium')
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# In[58]:
+
+
+# Set up the Safari WebDriver
+driver = webdriver.Safari()
+
+#connect to website
+    
+URL='https://www.amazon.com/funny-analyst-definition-scientist-t-shirt/dp/B07NLP2PKY/ref=sr_1_4?crid=IYUM6BNZOA9I&dib=eyJ2IjoiMSJ9.PM2zhQFx_lfPnddZwr05DvJ8zxTiKbMNof3H_8ObTes8YarT96m4jiSWTM-p0IE3bA2qY9UPmB77hgju1FjQdzoGTg9Z9RCoiq9k-QHkViQOvTyZAA-_97_tVX9KJmI4FEwuFkUjYmo-JXkjcqp2P6u3dh89brhiDetURS8hTpn4R6oUt6SHbBSapqx2Ln-bx-5Ccuee5XhzWDSZZwnwtMfCQIB3vQMZNUs78e62AeIJ1v9NvvOPsnKMAe2SQWdOD-kBkATixwK7JUzEV_4SNMW3msbHZV0_rQ7BBZ6e4iw.r0fDge_RYbaEYjv4seJoPU6BZ5ChEhC6rfg9GFD2TzE&dib_tag=se&keywords=data%2Banalyst%2Btshirt&qid=1721596729&sprefix=data%2Banalyst%2B%2Caps%2C95&sr=8-4'
+
+driver.get(URL)
+
+
+#print(soup1)
+
+
+# In[59]:
+
+
+# Get the page content
+page_content = driver.page_source
+
+# Close the WebDriver
+driver.quit()
+
+# Use BeautifulSoup to parse the page content
+soup1 = BeautifulSoup(page_content, 'html.parser')
+
+
+# In[60]:
+
+
+soup2= BeautifulSoup(soup1.prettify(),'html.parser')
+print(soup2)
+
+
+# In[61]:
+
+
+title= soup2.find(id='productTitle').get_text().strip() 
+
+print(title)
+
+
+# In[62]:
+
+
+price= soup2.find(id='corePriceDisplay_desktop_feature_div').get_text().strip().split('\n')[0][1:]
+
+
+print(price)
+
+
+# In[63]:
+
+
+today= datetime.date.today()
+print(today)
+
+
+# In[64]:
+
+
+# Create CSV and write headers and data into the file
+
+header = ['Title', 'Price', 'Date']
+data = [title, price, today]
+
+
+with open('/Users/asharani/Desktop/Study material/AmazonWebScraperDataset.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(data)
+
+
+# In[65]:
+
+
+#Now we are appending data to the csv
+
+with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(data)
+
+
+# In[66]:
+
+
+#Combine all of the above code into one function
+
+
+def check_price():
+    driver = webdriver.Safari()
+    URL = 'https://www.amazon.com/funny-analyst-definition-scientist-t-shirt/dp/B07NLP2PKY/ref=sr_1_4?crid=IYUM6BNZOA9I&dib=eyJ2IjoiMSJ9.PM2zhQFx_lfPnddZwr05DvJ8zxTiKbMNof3H_8ObTes8YarT96m4jiSWTM-p0IE3bA2qY9UPmB77hgju1FjQdzoGTg9Z9RCoiq9k-QHkViQOvTyZAA-_97_tVX9KJmI4FEwuFkUjYmo-JXkjcqp2P6u3dh89brhiDetURS8hTpn4R6oUt6SHbBSapqx2Ln-bx-5Ccuee5XhzWDSZZwnwtMfCQIB3vQMZNUs78e62AeIJ1v9NvvOPsnKMAe2SQWdOD-kBkATixwK7JUzEV_4SNMW3msbHZV0_rQ7BBZ6e4iw.r0fDge_RYbaEYjv4seJoPU6BZ5ChEhC6rfg9GFD2TzE&dib_tag=se&keywords=data%2Banalyst%2Btshirt&qid=1721596729&sprefix=data%2Banalyst%2B%2Caps%2C95&sr=8-4'
+    driver.get(URL)
+    page_content = driver.page_source
+    driver.quit()
+
+    soup1 = BeautifulSoup(page_content, 'html.parser')
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title= soup2.find(id='productTitle').get_text().strip() 
+
+    price= soup2.find(id='corePriceDisplay_desktop_feature_div').get_text().strip().split('\n')[0][1:]
+    
+    today= datetime.date.today()
+    
+    header = ['Title', 'Price', 'Date']
+    data = [title, price, today]
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
+# In[67]:
+
+
+while(True):
+    check_price()
+    time.sleep(10)
+
+
+# In[68]:
+
+
+df = pd.read_csv('/Users/asharani/Desktop/Study material/AmazonWebScraperDataset.csv')
+
+print(df)
+
+
+# In[69]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
